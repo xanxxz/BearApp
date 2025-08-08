5a8ad74db87ff78cb092dc1b0d09c914bd1b48d7
unknown <boneislandman00@gmail.com>
2025-08-08 19:21:57 +0400

    rebase

---
+++ src/components/protected-route/protected-route.tsx
@@ -0,0 +1,7 @@
+// import { Navigate, useLocation } from 'react-router-dom';
+
+type ProtectedRouteProps = { };
+
+export const ProtectedRoute = ({ }: ProtectedRouteProps) => {
+
+};
---
+++ src/components/ui/app-header/appHeader.tsx
@@ -0,0 +1,6 @@
+// import React, { FC } from 'react';
+// import styles from './app-header.css';
+// import { TAppHeaderUIProps } from './type';
+// import { Link, NavLink } from 'react-router-dom';
+
+// export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ }) => ( );
